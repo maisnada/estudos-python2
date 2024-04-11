08dd004477021730db44e191dd17a618c46613a4
Marcelo Moreno <marcelo.moreno@univali.br>
2024-04-10 22:14:19 -0300

    adição app

---
+++ app.py
@@ -0,0 +1,11 @@
+print('-- Sabor Express --\n')
+
+print('1 - Cadastrar restaurante')
+print('2 - Listar restaurante')
+print('1 - Ativar restaurante')
+print('1 - Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+
+print('\nVocê escolheu a opção: ', opcao_escolhida)
+
