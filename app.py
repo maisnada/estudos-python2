274444cb43b81dcffdb432c47dbe4832fed1210b
Marcelo Moreno <marcelo.moreno@univali.br>
2024-04-10 23:44:50 -0300

    funções

--- app.py
+++ app.py
@@ -1,11 +1,48 @@
-print('-- Sabor Express --\n')
+import os
+
+def finalizar_app():
+    
+    os.system('cls')
+    
+    print('Finalizando o app ^.^💀\n')
+
+print('\x1b[6;30;42m' + '''
+╔═══╗──╔╗───────╔═══╗
+║╔═╗║──║║───────║╔══╝
+║╚══╦══╣╚═╦══╦═╗║╚══╦╗╔╦══╦═╦══╦══╦══╗
+╚══╗║╔╗║╔╗║╔╗║╔╝║╔══╩╬╬╣╔╗║╔╣║═╣══╣══╣
+║╚═╝║╔╗║╚╝║╚╝║║─║╚══╦╬╬╣╚╝║║║║═╬══╠══║
+╚═══╩╝╚╩══╩══╩╝─╚═══╩╝╚╣╔═╩╝╚══╩══╩══╝
+───────────────────────║║
+───────────────────────╚╝\n'''+ '\x1b[0m')
 
 print('1 - Cadastrar restaurante')
 print('2 - Listar restaurante')
 print('1 - Ativar restaurante')
 print('1 - Sair\n')
 
-opcao_escolhida = input('Escolha uma opção: ')
+opcao_escolhida = int(input('Escolha uma opção: '))
 
-print('\nVocê escolheu a opção: ', opcao_escolhida)
+if opcao_escolhida == 1:
+    
+    print('Cadastrar restaurante')
+    
+elif opcao_escolhida == 2:
+    
+    print('Listar restaurantes')
+
+elif opcao_escolhida == 3:
+    
+    print('Ativar restaurante')
+    
+else:
+    
+    finalizar_app()
+
+#print(f'\nVocê escolheu a opção: {opcao_escolhida}')
+#print(type(opcao_escolhida))
+
+#exemplo formatação float
+#print(f'O valor arredondado de pi é: {pi:.2f}')
+
 
